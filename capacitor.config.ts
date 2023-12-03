cb464aad58e7ab04efeb58686211c9fd2716a3cc
cosmos-23 <cosmos1012@tutanota.com>
2023-12-03 09:49:18 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_react_stripe',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
